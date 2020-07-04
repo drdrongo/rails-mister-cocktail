91fe789b8bbc40dba7ed1c60a7432d96c64a3e46
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-04 17:31:57 +0900

    back end

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
     @doses = Dose.where(cocktail_id: params[:id])
   end
 
@@ -15,7 +16,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -37,6 +38,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require('cocktail').permit(:name)
+    params.require(:cocktail).permit(:name)
   end
 end
