bd845dc526a7c7f6a5b382af5862a0e0c40129e6
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-09 21:00:20 +0900

    cards changed, typed added, navbar altered

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,29 @@
+import Typed from 'typed.js';
+
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: [
+    "mojito",
+    "007 Martini",
+    "vermouth",
+    "Long Island Iced Tea",
+    "screwdriver",
+    "saké",
+    "caipiroska",
+    "rum",
+    "tropical itch",
+    "gin",
+    "metropolis",
+    "Absinthe",
+    "tuxedo",
+    "Tequila",
+    "Ice Pick",
+    "sherry",
+    ],
+    typeSpeed: 100,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
